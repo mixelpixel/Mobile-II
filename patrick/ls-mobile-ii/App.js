22107ddfb688a7d94704a02fccdd3991c00ae94b
mixelpixel <mixelpix@mac.com>
2017-09-11 23:49:55 -0400

    View architecture

--- patrick/ls-mobile-ii/App.js
+++ patrick/ls-mobile-ii/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import 'expo'
+// import 'expo' // added this during set-up funk. Don't think I need it...
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        {/* <Home />
+        <SignIn />
+        <SignUp />
+        <Content /> */}
       </View>
     );
   }

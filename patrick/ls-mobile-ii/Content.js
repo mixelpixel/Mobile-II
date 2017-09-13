9e0474593070bbb2cbb117e005c654a7e03260cb
mixelpixel <mixelpix@mac.com>
2017-09-13 06:14:01 -0400

    Content

--- patrick/ls-mobile-ii/Content.js
+++ patrick/ls-mobile-ii/Content.js
@@ -3,16 +3,16 @@ import {
   StyleSheet,
   Text,
   View,
+  AsyncStorage,
   FlatList,
 } from 'react-native';
 import axios from 'axios';
-
 
 export default class Content extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: [],
+      users: []
     };
   }
 
@@ -21,22 +21,27 @@ export default class Content extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://mobile-server-ii.herokuapp.com/users').then((response) => {
-      this.setState({
-        users: response,
-      });
+    AsyncStorage.getItem('token').then((token) => { // retrieve the token from "localStorage"
+    axios.get('https://mobile-server-ii.herokuapp.com/users', {
+      headers: {
+        authorization: token, // attach the token as a header
+      }
+    }).then((response) => {
+      // Update state in here
+      this.setState({ users: response.data });
     });
-  }
+  })
+ }
 
-  render() {
-    return (
-      <View>
-        <FlatList
-          data={this.state.users}
-          renderItem={({ item }) => {
-            return <Text>{item.text}</Text>;
-          }}/>
-      </View>
-    );
-  }
+ render() {
+  return (
+    <View>
+      <FlatList
+        data={this.state.users}
+        renderItem={({item}) => <Text>{ item.email }</Text>}
+        keyExtractor= { item => item._id }
+      />
+    </View>
+  );
+ }
 }
